3f8fca64de0fa1d2259b0ee0a05f6067d4e6c2b5
Tim Jansen <jansent16@gmail.com>
2018-12-10 15:56:45 +0100

    Create MyGame.h

---
+++ MyGame.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include <QStackedwidget>
+#include <MenuScreen.h>
+#include <QHBoxLayout>
+#include <ClassicGameScreen.h>
+#include <QTimer>
+#include <QDebug>
+#include <QElapsedtimer>
+#define GAME_HEIGHT 768
+#define GAME_ASPECT_RATIO 9/16
+#define GAME_DELTA_TIME 16
+
+class SnakeVsBlock : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	//constructor
+	SnakeVsBlock(QWidget *parent = Q_NULLPTR);
+
+private:
+	//vars
+	QStackedWidget *gameScreens;
+	QTimer *gameTimer;
+	QElapsedTimer *gameElTimer;
+	qint64 lastUpdateTime, nowUpdateTime = 0;
+	float deltaTime;
+	int GameH;
+	int GameW;
+
+	//methods
+	void startGameLoop();
+
+private slots:
+	//methods
+	void update();
+};
